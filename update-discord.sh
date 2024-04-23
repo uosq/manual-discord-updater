972bf5040eb661b4c0c4c34ce42feb059b8c1b72
navet <159667110+uosq@users.noreply.github.com>
2024-04-22 21:30:08 -0300

    Update update-discord.sh

--- update-discord.sh
+++ update-discord.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0]
+# https://stackoverflow.com/questions/18215973/how-to-check-if-running-as-root-in-a-bash-script
+if [ "$EUID" -ne 0 ]
 	then echo "Please run as root"
 	exit
 fi
